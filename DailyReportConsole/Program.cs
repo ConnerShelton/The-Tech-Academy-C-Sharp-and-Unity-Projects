1234908a0f5b77d524781e6ad17c1bad196fbbf1
Conner Shelton <clshelt24@outlook.com>
2021-07-08 16:23:45 -0700

    Add Report file from VS
    This is the daily report file made using the Console App framework.

---
+++ DailyReportConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReportConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Daily Report");
+
+            Console.WriteLine("Please press enter to begin");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            string yourCourse = Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Do you need help with anything? Please answer true or false.");
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string goodExp = Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string feedBack = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            int timeStudied = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
