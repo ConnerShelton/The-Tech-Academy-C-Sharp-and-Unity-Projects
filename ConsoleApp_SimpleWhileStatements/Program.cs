a6d76238338015e7e46b805f36c375796b1e5fac
Conner Shelton <clshelt24@outlook.com>
2021-07-13 03:22:11 -0700

    submit while statements

---
+++ ConsoleApp_SimpleWhileStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_SimpleWhileStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int q = 0;
+            //while (q < 5)
+            //{
+            //    Console.WriteLine(q);
+            //    q++;
+            //}
+
+            //Console.ReadLine();
+
+            int z = 0;
+            do
+            {
+                Console.WriteLine(z);
+                z++;
+            }
+            while (z < 5);
+
+            Console.ReadLine();
+        }
+    }
+}
