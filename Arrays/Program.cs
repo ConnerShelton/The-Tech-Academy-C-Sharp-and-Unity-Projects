3dabe9f14f695e467dca5281b1650e4775f32cd4
Conner Shelton <clshelt24@outlook.com>
2021-07-21 15:31:55 -0700

    fix array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -14,97 +14,63 @@ namespace Arrays
 
             int[] myNum = { 15, 25, 35, 45 };
 
-            //initialize list
-            List<string> intList = new List<string>();
-            //Create list items
-            intList.Add("Hello");
-            intList.Add("Conner");
-            intList.Remove("Conner");
-
-            //Instructions for data use
-            Console.WriteLine("Please input the location code of the data you need.");
-            Console.WriteLine("Input should be a 'Base' and 'Item' number such as: 0-0");
-            Console.WriteLine("'Bases': Cars = 0 ; Numbers = 1");
-            Console.WriteLine("'Items(0)': Volvo = 0 ; BMW = 1 ; Ford = 2 ; Mazda = 3");
-            Console.WriteLine("'Items(1)': 15 = 0 ; 25 = 1 ; 35 = 2 ; 45 = 3");
-            string useArray = Console.ReadLine();
-
-            //Cars array items
-            if (useArray == "0-0")
+            //CAR ARRAY
+            Console.WriteLine("Please input the Index location code of the data you need.");
+            int carIndex = Convert.ToInt32(Console.ReadLine());
+            if (carIndex >= 0 && carIndex <= 3)
             {
-                Console.WriteLine(cars[0]);
-                Console.ReadLine();
-            }
-            else if (useArray == "0-1")
-            {
-                Console.WriteLine(cars[1]);
-                Console.ReadLine();
-            }
-            else if (useArray == "0-2")
-            {
-                Console.WriteLine(cars[2]);
-                Console.ReadLine();
-            }
-            else if (useArray == "0-3")
-            {
-                Console.WriteLine(cars[3]);
-                Console.ReadLine();
-            }
-
-            //Number array items
-            else if (useArray == "1-0")
-            {
-                Console.WriteLine(myNum[0]);
-                Console.ReadLine();
-            }
-            else if (useArray == "1-1")
-            {
-                Console.WriteLine(myNum[1]);
-                Console.ReadLine();
-            }
-            else if (useArray == "1-2")
-            {
-                Console.WriteLine(myNum[2]);
-                Console.ReadLine();
-            }
-            else if (useArray == "1-3")
-            {
-                Console.WriteLine(myNum[3]);
+                Console.WriteLine(cars[carIndex]);
                 Console.ReadLine();
             }
 
             //No data available msg
             else
             {
-                Console.WriteLine("The location code you selected does not seem to exist!");
+                Console.WriteLine("Sorry but the number you selected does not seem to exist!");
                 Console.WriteLine("Have a Good Day!!!");
                 Console.ReadLine();
             }
 
-            //Instructions for list use
-            Console.WriteLine("Please input the location code of the data you need.");
-            Console.WriteLine("Input should be a number such as: 3");
-            string useList = Console.ReadLine();
+            //NUM ARRAY
 
-            //List items
-            if (useList == "0")
+            Console.WriteLine("Please input the Index location code of the data you need.");
+            int numIndex = Convert.ToInt32(Console.ReadLine());
+            if (numIndex >= 0 && numIndex <= 3)
             {
-                Console.WriteLine(intList[0]);
+                Console.WriteLine(myNum[numIndex]);
                 Console.ReadLine();
             }
-            else if (useList == "1")
-            {
-                Console.WriteLine(intList[1]);
-                Console.ReadLine();
-            }
-            else if (useList == "2")
-            {
-                Console.WriteLine(intList[2]);
-                Console.ReadLine();
-            }
+
+            //No data available msg
             else
             {
-                Console.WriteLine("The location code you selected does not seem to exist!");
+                Console.WriteLine("Sorry but the number you selected does not seem to exist!");
+                Console.WriteLine("Have a Good Day!!!");
+                Console.ReadLine();
+            }
+
+            //LIST
+
+            List<string> intList = new List<string>();
+
+            intList.Add("Hello");
+            intList.Add("Conner");
+            intList.Add("Boo");
+            //list use
+            Console.WriteLine("Please input the location code of the data you need.");
+            Console.WriteLine("Input should be a number such as: 2");
+
+            int listIndex = Convert.ToInt32(Console.ReadLine());
+            if (listIndex >= 0 && listIndex <= 2)
+            {
+                Console.WriteLine(intList[listIndex]);
+                Console.ReadLine();
+            }
+
+            //No data available msg
+            else
+            {
+                Console.WriteLine("Sorry but the number you selected does not seem to exist!");
                 Console.WriteLine("Have a Good Day!!!");
                 Console.ReadLine();
             }
