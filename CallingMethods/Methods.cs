727fd56b5328a2ad2d56d2db9c7264c0719115ed
Conner Shelton <clshelt24@outlook.com>
2021-09-04 17:05:34 -0700

    Submit calling methods
    very simple equations being called from different methods

---
+++ CallingMethods/Methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Methods
+    {
+        public void Multiply(int userIn)
+        {
+            Console.WriteLine(userIn * 8);
+        }
+
+        public void Divide(int userIn)
+        {
+            Console.WriteLine(userIn / 3);
+        }
+
+        public void Add(int userIn)
+        {
+            Console.WriteLine(userIn + 100);
+        }
+    }
+}
