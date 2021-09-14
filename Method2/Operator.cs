8b0298b5b7dbf407421fd8dfdd0653c7eb36388a
Conner Shelton <clshelt24@outlook.com>
2021-09-13 20:10:14 -0700

    Submit new method

---
+++ Method2/Operator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Operator
+    {
+        public static void Add(int x)
+        {
+            int answer = x + 15;
+            Console.WriteLine(answer);
+        }
+
+        public static void Add(int x, int y)
+        {
+            int answer = x + y;
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Method2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first variable of your choice(Cannot be blank)");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second variable of your choice(Can be blank)");
+            string Y = Console.ReadLine();
+
+            if (int.TryParse(Y, out int y))
+            {
+                Operator.Add(x, y);
+            }
+            else
+            {
+                Operator.Add(x);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
