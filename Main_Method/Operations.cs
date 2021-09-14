4f00f4fd7daac3977263cf40fdbd0d61d07ec960
Conner Shelton <clshelt24@outlook.com>
2021-09-13 18:16:35 -0700

    Main Method Submit
    Submit of the Main Method assignment from step 218

---
+++ Main_Method/Operations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    public class Operations
+    {
+        public static void Add(int x, int y)
+        {
+            int answer = x + y;
+            Console.WriteLine(answer);
+        }
+
+        public static void Add(decimal x, decimal y)
+        {
+            decimal answer = x + y;
+            Console.WriteLine("$" + answer);
+        }
+
+        public static void Add(string x, string y)
+        {
+            int X = Convert.ToInt32(x);
+            int Y = Convert.ToInt32(y);
+
+            int answer = X + Y;
+            Console.WriteLine(answer);
+        }
+    }
+}
---
+++ Main_Method/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations.Add(3, 7);
+
+            Operations.Add(9.25m, 5.75m);
+
+            Operations.Add("7", "13");
+
+            Console.ReadLine();
+        }
+    }
+}
