cec29e0e376cf2b11e1541d77a2a653c150e8224
Conner Shelton <clshelt24@outlook.com>
2021-09-17 14:30:54 -0700

    Assignment submit
    Method Class assignment submission

---
+++ method_Class/MthClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_Class
+{
+    public class MthClass
+    {
+        public static void Add(int int1, int int2)
+        {
+            int answer = int1 + 7;
+            Console.WriteLine(answer);
+            Console.WriteLine(int2);
+        }
+    }
+}
---
+++ method_Class/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MthClass name = new MthClass();
+            MthClass.Add(int1: 13, int2: 20);
+
+            Console.ReadLine();
+        }
+    }
+}
