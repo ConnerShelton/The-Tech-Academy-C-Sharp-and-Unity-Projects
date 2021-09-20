1f9dfbd7b29d809bc710736df318297dded92b38
Conner Shelton <clshelt24@outlook.com>
2021-09-20 14:18:52 -0700

    Class assignment submit

---
+++ Class_Submission/Method.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission
+{
+    public class Method
+    {
+        public static void Divide(string userIn)
+        {
+            int num = Convert.ToInt32(userIn);
+            int answer = num / 2;
+            Console.WriteLine(answer);
+        }
+
+        public static void Divide(string userIn, int myIn)
+        {
+            Console.WriteLine("User input was not an integer");
+            Console.WriteLine("Here is a number to cover for you: " + myIn);
+            int num = myIn;
+            int answer = num / 2;
+            Console.WriteLine(answer);
+        }
+    }
+
+    
+}
---
+++ Class_Submission/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Submission
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //int userIn = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            Console.Write("Please Enter a Number: ");
+            string userIn = Console.ReadLine(); //get input
+            int myIn = 0;
+
+            Method name = new Method();
+
+            if (!int.TryParse(userIn, out myIn))
+            {
+                Method.Divide(userIn, 18);
+            }
+            else
+            {
+                Method.Divide(userIn);
+            }
+
+            //if (!Int32.IsNumber(userIn))
+            //{
+            //    Method.Divide(userIn);
+            //}
+
+
+            Console.ReadLine();
+        }
+    }
+}
