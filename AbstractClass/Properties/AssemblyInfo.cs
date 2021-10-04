6889a6fda3682024f21d00e1bde6519991275008
Conner Shelton <clshelt24@outlook.com>
2021-10-04 16:50:34 -0700

    Submit fix

--- AbstractClass/Properties/AssemblyInfo.cs
+++ AbstractClass/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("mthdsAndObjects")]
+[assembly: AssemblyTitle("AbstractClass")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("mthdsAndObjects")]
+[assembly: AssemblyProduct("AbstractClass")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9d1d1611-5729-4341-b082-c1e17aefb04f")]
+[assembly: Guid("aded7327-4482-4071-a9dc-52049ab00878")]
 
 // Version information for an assembly consists of the following four values:
 //
