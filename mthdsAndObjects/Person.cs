27286a29bcb2658d575c3f9020525a82b116a322
Conner Shelton <clshelt24@outlook.com>
2021-10-04 12:58:18 -0700

    submit start of assignment
    got down the needed properties, then got lost trying to find references to the "Name: [full name]" format.

---
+++ mthdsAndObjects/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mthdsAndObjects
+{
+    class Person
+    {
+        //Name propertys
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //method SayName
+        public static void SayName()
+        {
+            
+        }
+    }
+}
