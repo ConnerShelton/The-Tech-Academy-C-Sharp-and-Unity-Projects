4d56bfe755d194799c1518d7ba6ed61919d8ae49
Conner Shelton <clshelt24@outlook.com>
2021-10-04 16:21:38 -0700

    submit abstract  class assignment

---
+++ AbstractClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee.firstName = "Sample";
+            Employee.lastName = "Student";
+
+            Employee employee = new Employee();
+
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
